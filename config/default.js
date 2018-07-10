17a2d34b14311a42b4d8e50b014dbed0a68f9132
maria.brinas-dobrowski <maria.brinas-dobrowski@target.com>
2018-07-10 16:17:09 -0500

    updated the config.json

--- config/default.js
+++ config/default.js
@@ -1,7 +1,7 @@
 module.exports = {
    appId           : '29172a9c-c893-456a-bc8c-648132e35390',
-   clientId        : 'myclientid',
-   clientSecret    : 'myclientsecret',
-   appSignature    : 'myappsignature',
+   clientId        : 'g2dv103gequ2pom59xgr6ggv',
+   clientSecret    : 'QQfGf8llNqTh9n0vNMBllToA',
+   appSignature    : 'poc_journey',
    authUrl         : 'https://auth.exacttargetapis.com/v1/requestToken?legacy=1'
 };
