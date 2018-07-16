7beebcdfcdf3eed7d2dae53a212f7cde4ce28c09
maria.brinas-dobrowski <maria.brinas-dobrowski@target.com>
2018-07-16 12:40:31 -0500

    changed appid

--- config/default.js
+++ config/default.js
@@ -1,5 +1,5 @@
 module.exports = {
-   appId           : '29172a9c-c893-456a-bc8c-648132e35390',
+   appId           : '6a76f69a-6e10-4158-89b6-c09dc2bcbfbe',
    clientId        : 'g2dv103gequ2pom59xgr6ggv',
    clientSecret    : 'QQfGf8llNqTh9n0vNMBllToA',
    appSignature    : 'poc_journey',
