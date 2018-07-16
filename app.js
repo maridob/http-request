383b26fcf15758babedc7022e8d9f13372b09bbb
maria.brinas-dobrowski <maria.brinas-dobrowski@target.com>
2018-07-16 12:50:24 -0500

    changed http-trigger to app id

--- app.js
+++ app.js
@@ -125,7 +125,7 @@ app.post('/ixn/triggers/hello-world/', trigger.edit );
 // Abstract Event Handler
 app.post('/fireEvent/:type', function( req, res ) {
     var data = req.body;
-    var triggerIdFromAppExtensionInAppCenter = 'http-trigger';
+    var triggerIdFromAppExtensionInAppCenter = '6a76f69a-6e10-4158-89b6-c09dc2bcbfbe';
     var JB_EVENT_API = 'https://www.exacttargetapis.com/interaction-experimental/v1/events';
     var reqOpts = {};
 
