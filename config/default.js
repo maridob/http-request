1ad59a710f3118f9d1fbe63b802fcb97bfbbb50f
maria.brinas-dobrowski <maria.brinas-dobrowski@target.com>
2018-07-17 09:38:00 -0500

    delete config

--- config/default.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-   appId           : 'f1406e8e-aaa3-44bf-aafb-080915aac4d4',
-   clientId        : 'g2dv103gequ2pom59xgr6ggv',
-   clientSecret    : 'QQfGf8llNqTh9n0vNMBllToA',
-   appSignature    : 'poc_journey',
-   authUrl         : 'https://auth.exacttargetapis.com/v1/requestToken?legacy=1'
-};
